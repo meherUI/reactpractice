f82a7a52d6cd6dc71e4864c800fad34cbf4331f2
unknown <tmkrishna.ui@gmail.com>
2020-04-24 16:02:04 +0530

    "Basic Modal Popup"

--- src/App.js
+++ src/App.js
@@ -2,25 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import ModalPopUp from './components/ModalPopUp'
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      showPopup: false
+    }
+    this.handleClick.bind(this)
+  }
+  handleClick= () =>{
+    this.setState({
+      showPopup: !this.state.showPopup
+    })
+  }
+  render(){
+    return (
+      <div className="App">
+        <button onClick={this.handleClick}>Modal</button>
+        {this.state.showPopup && <ModalPopUp header="Modal Popup">
+            {console.log("close",this.handleClick)}
+            <p>Model Content</p>
+            <h4>Modal Footer</h4>
+
+            <button onClick={this.handleClick}>Close</button>
+                  
+            </ModalPopUp>}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/ModalPopUp.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class ModalPopUp extends Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return (
+            <div className="popup">
+                <div className="popupBody">
+                    <h2>{this.props.header}</h2>
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+}
